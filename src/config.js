48b91fd010ab6ed514cef7540806b35e2b5b1c52
Francisco Rodríguez <frodriguez25@ucol.mx>
2023-05-22 20:34:19 -0600

    Detalles finales Paco

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config()
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test"
 export const MONGODB_URI__RESTAURANTS = process.env.MONGODB_URI__RESTAURANTS || "mongodb://127.0.0.1:27017/test-restaurant"
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 4000
